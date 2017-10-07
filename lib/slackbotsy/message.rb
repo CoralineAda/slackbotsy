ca41bc49d9f5b9c3dde130f41bb2a658c7394f13
CoralineAda <coraline@idolhands.com>
2017-10-07 13:06:44 -0500

    Wait, does display_name just work?

--- lib/slackbotsy/message.rb
+++ lib/slackbotsy/message.rb
@@ -35,7 +35,7 @@ module Slackbotsy
     end
 
     ## convenience getter methods for message properties
-    %w[ token team_id channel_id channel_name timestamp user_id user_name user_display_name displayname text ].each do |method|
+    %w[ token team_id channel_id channel_name timestamp user_id user_name display_name text ].each do |method|
       define_method(method) do
         self.fetch(method, nil)
       end
