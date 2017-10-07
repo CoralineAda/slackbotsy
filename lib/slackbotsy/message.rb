000b95ebdc4b6363bf88a6968b292c893d26d4bd
CoralineAda <coraline@idolhands.com>
2017-10-07 12:54:33 -0500

    Naive approach to see if this just works

--- lib/slackbotsy/message.rb
+++ lib/slackbotsy/message.rb
@@ -34,7 +34,7 @@ module Slackbotsy
     end
 
     ## convenience getter methods for message properties
-    %w[ token team_id channel_id channel_name timestamp user_id user_name text ].each do |method|
+    %w[ token team_id channel_id channel_name timestamp user_id user_name display_name text ].each do |method|
       define_method(method) do
         self.fetch(method, nil)
       end
